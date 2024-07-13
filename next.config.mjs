a303a9e876be425715feaf77e58d52a4788981f5
RDX-Rudra <dasrudra234@gmail.com>
2024-07-14 02:15:14 +0530

    Next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["@heroicons/react"],
-      },
-      reactStrictMode: true,
-      swcMinify: true,
+    // experimental: {
+    //     serverComponentsExternalPackages: ["@heroicons/react"],
+    //   },
+    //   reactStrictMode: true,
+    //   swcMinify: true,
 };
 
 export default nextConfig;
